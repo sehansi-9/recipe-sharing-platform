ea5c569a192858cbc945c7086e86e4fae61d763b
sehansi-9 <sehansi.wsmp9@gmail.com>
2025-04-07 23:08:34 +0530

    create favourites page

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import "./css/App.css";
+import Favorites from "./pages/Favorites";
 import Home from "./pages/Home";
 import { Routes, Route } from "react-router-dom";
 import { RecipeProvider } from "./contexts/RecipeContext";
@@ -10,7 +11,7 @@ function App() {
       <main className="main-content">
         <Routes>
           <Route path="/" element={<Home />} />
-          
+          <Route path="/favorites" element={<Favorites />} />
         </Routes>
       </main>
     </RecipeProvider>
---
+++ src/pages/Favorites.jsx
@@ -0,0 +1,28 @@
+import { useRecipeContext } from "../contexts/RecipeContext";
+import RecipeCard from "../components/RecipeCard";
+
+function Favorites() {
+  const { favorites } = useRecipeContext();
+
+  if (favorites) {
+    return (
+      <div className="favorites">
+        <h2>Your Favorites</h2>
+        <div className="recipe-grid">
+          {favorites.map((recipe) => (
+            <RecipeCard recipe={recipe} key={recipe.id} />
+          ))}
+        </div>
+      </div>
+    );
+  }
+
+  return (
+    <div className="favorites-empty">
+      <h2>No Favorite Recipes Yet</h2>
+      <p>Start adding recipes to your favorites and they will appear here!</p>
+    </div>
+  );
+}
+
+export default Favorites;
