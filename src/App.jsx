8af82f42b2e046ecbe7875e85fffec63ba5ce13f
sehansi-9 <sehansi.wsmp9@gmail.com>
2025-04-07 23:10:35 +0530

    add navbar component

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,12 @@ import Favorites from "./pages/Favorites";
 import Home from "./pages/Home";
 import { Routes, Route } from "react-router-dom";
 import { RecipeProvider } from "./contexts/RecipeContext";
-
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
     <RecipeProvider>
+      <NavBar />
       <main className="main-content">
         <Routes>
           <Route path="/" element={<Home />} />
